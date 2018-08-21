23829710b36489817816b8766f8acfef36703436
ThinhVuVentura <thinhvd@zigexn.vn>
2018-08-21 08:37:08 +0700

    UI devise user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	def check_active_controller(con_name)
+		con_name.each do |con|
+			if con == controller_name
+				return true
+			end
+		end
+		return false
+	end
+
+	def check_active_action(act_name)
+		act_name.each do |act|
+			if act == action_name
+				return true
+			end
+		end
+		return false
+	end
+
+	def alert_class(key)
+    key == 'notice' ? 'success' : 'danger'
+  end
 end
